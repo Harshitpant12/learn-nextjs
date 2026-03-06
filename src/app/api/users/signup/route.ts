0383762b129fc1daba670bee300d13b4c06f1cf2
Harshitpant12 <harshpant778@gmail.com>
2026-03-06 20:26:38 +0530

    implement signup functionality on backend

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,47 @@
+import {connect} from "@/dbConfig/dbConfig"
+import User from "@/models/userModel"
+import {NextRequest, NextResponse} from "next/server"
+import bcrypt from "bcryptjs"
+
+
+connect()
+
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody
+
+        if (!username || !email || !password) {
+            return NextResponse.json({message: "All fields are required"}, {status: 400})
+        }
+        console.log(reqBody) // remove it later
+
+        const user = await User.findOne({email})
+        if (user) {
+            return NextResponse.json({message: "User already exists"}, {status: 400})
+        }
+
+        //hash the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        const savedUser = await newUser.save()
+        console.log(savedUser);
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser
+        })
+
+    } catch (error: any) {
+        return NextResponse.json({message: error.message}, {status: 500})
+    }
+}
