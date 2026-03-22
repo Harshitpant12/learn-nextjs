34e489f1d98752194d80484d9ec5ff9b7fbd490a
Harshitpant12 <harshpant778@gmail.com>
2026-03-22 16:53:35 +0530

    implement /me route functionality for user details and update profile page to get user id

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,23 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request: NextRequest) {
+    try {
+        const userId = getDataFromToken(request);
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+        return NextResponse.json({
+            message: "User found",
+            data: user,
+            status: 200,
+        });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,11 +2,13 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
 
   const router = useRouter()
+  const [data, setData] = useState("nothing")
   const logout = async () => {
     try {
       await axios.get('/api/users/logout')
@@ -18,13 +20,21 @@ export default function ProfilePage() {
     }
   }
 
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log(res.data);
+    setData(res.data.data._id)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-4">Profile Page</h1>
+      <h2>{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link> }</h2>
       <hr />
       <p className="text-lg text-gray-600">This is the profile page.</p>
       <hr />
       <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+      <button onClick={getUserDetails} className="bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Get User Details</button>
     </div>
   );
 }
